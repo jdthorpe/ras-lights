d1c832093c6e079f7688a50d51088385743550fe
Jason Thorpe <jdthorpe@gmail.com>
2021-12-25 20:57:53 -0800

    working controller

--- packages/web/controller/ws2812.py
+++ packages/web/controller/ws2812.py
@@ -6,7 +6,8 @@
 # various animations on a strip of NeoPixels.
 
 from rpi_ws281x import PixelStrip, Color
-from types import List
+from typing import List
+import random
 
 # LED strip configuration:
 LED_COUNT = 16  # Number of LED pixels.
@@ -14,7 +15,7 @@ LED_PIN = 18  # GPIO pin connected to the pixels (18 uses PWM!).
 # LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10  # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 100  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
@@ -28,16 +29,19 @@ def set_color_s(colors: List[List[int]]):
             COLORS.append(Color(**color))
     set_color(COLORS)
 
-def random_colors():
-    colors = []
-    for _ in range(strip.numPixels()):
-        a = random.randint(0, 100)
-        b = random.randint(0, 100)
-        colors.append([a, b, max(100 - a - b, 0)])
-    set_color_s(colors)
+OFF = Color(0,0,0)
 
-def set_color(colors: List[Color]):
-    """Draw rainbow that fades across all pixels at once."""
+def turn_off():
+    """Turn the lights off"""
+    if strip is None:
+        return
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, OFF)
+    strip.show()
+
+
+def set_color(color:List[Color]):
+    """Set the pixel colors (with recycling)"""
     if strip is None:
         return
     for i in range(strip.numPixels()):
@@ -45,8 +49,17 @@ def set_color(colors: List[Color]):
     strip.show()
 
 
-strip = None
+def random_colors():
+    """Set each pixel to a random color"""
+    colors = []
+    for _ in range(strip.numPixels()):
+        a = random.randint(0, 100)
+        b = random.randint(0, 100)
+        colors.append([a, b, max(100 - a - b, 0)])
+    set_color_s(colors)
 
+
+strip = None
 
 def start():
     global strip
@@ -69,31 +82,17 @@ def start():
 # Main program logic follows:
 if __name__ == "__main__":
     start()
-    import random
-
-    # set_color(
-    #     [Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))]
-    # )
-    import random
-
-    a = random.randint(0, 100)
-    b = random.randint(0, 100)
-    c = random.randint(0, 100)
-    d = random.randint(0, 100)
-    # set_color(
-    #     [
-    #         Color(a, b, max(100 - a - b, 0)),
-    #         Color(b, c, max(100 - b - c, 0)),
-    #         Color(c, d, max(100 - c - d, 0)),
-    #         Color(d, a, max(100 - d - a, 0)),
-    #     ]
-    # )
+    a = random.randint(0,100)
+    b = random.randint(0,100)
+    c = random.randint(0,100)
+    d = random.randint(0,100)
 
     set_color_s(
         [
             [a, b, max(100 - a - b, 0)],
             [b, c, max(100 - b - c, 0)],
-            [c, d, max(100 - c - d, 0)],
-            [d, a, max(100 - d - a, 0)],
+            {"red":c, "blue":d, "green":max(100 - c - d, 0)},
+            {"red":d, "blue":a, "green":max(100 - d - a, 0)},
         ]
     )
+
