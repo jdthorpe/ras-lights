ae18f5434404a7c0beb2a9263cb79286dd6c39a8
Jason Thorpe <jdthorpe@gmail.com>
2021-12-25 16:35:45 -0800

    working app

--- packages/api/controller/app.py
+++ packages/api/controller/app.py
@@ -1,7 +1,4 @@
 """
-A minimal examle of a worker which (1) recieves work requests via http
-(aiohttp) executes database fetch / upate asynchronously in the main process,
-and does work in a pool of sub-process.
 """
 # pylint: disable=invalid-name
 from aiohttp import web
@@ -11,25 +8,46 @@ from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 import os.path
 
-# from ws2812 import start, set_color_s, random_colors
+from ws2812 import start, set_color_s, random_colors, turn_off
+
+@web.middleware
+async def debug_middleware(request, handler):
+    try:
+        return await handler(request)
+    except web.HTTPException as ex:
+        print("Error {} in request for {}".format(ex.status,request.path))
+        raise
+    except:
+        print("Something went wront in request for {}".format(request.path))
+        raise
+
+
 
 
 def main():
     """
     start the server
     """
-    # start()
+    start()
     with open(
         os.path.join(os.path.dirname(os.path.abspath(__file__)), "schema.yaml")
     ) as f:
         schema = yaml.safe_load(f)
 
+    async def get_turn_off(request):
+        """
+        set random colors
+        """
+        print("turn off")
+        turn_off()
+        return web.Response(text="OK")
+
     async def get_random(request):
         """
         set random colors
         """
-        # random_colors()
         print("random colors")
+        random_colors()
         return web.Response(text="OK")
 
     async def post_color(request):
@@ -47,12 +65,16 @@ def main():
             raise web.HTTPBadRequest(reason="Payload does not meet the schema")
 
         print("set colors: {}".format(data))
+        set_color_s(data)
         return web.Response(text="OK")
-        # set_color_s(data)
 
     app = web.Application()
+    app.middlewares.append(debug_middleware)
     app.add_routes(
-        [web.get("/random", get_random), web.post("/set-colors", post_color)]
+        [
+            web.get("/random", get_random), 
+        web.get("/off", get_turn_off), 
+        web.post("/set-colors", post_color)]
     )
     web.run_app(app, port=4444)
 
