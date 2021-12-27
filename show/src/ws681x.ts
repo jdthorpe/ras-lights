3717037b87b5599cdde83be99fc1bcc2f435ef0c
Jason Thorpe <jdthorpe@gmail.com>
2021-12-27 14:13:54 -0800

    testing

--- show/src/ws681x.ts
+++ show/src/ws681x.ts
@@ -6,12 +6,17 @@ ws281x.configure(settings.ws281x);
 export type rgb = [number, number, number];
 
 export function set_colors(colors: rgb[]): void {
+    console.log("settings.LEDS: ", settings.LEDS);
     const pixels = new Uint32Array(settings.LEDS);
-    for (let i = 0; i < settings.LEDS; i++)
+    console.log("pixels.length: ", pixels.length);
+    for (let i = 0; i < settings.LEDS; i++) {
+        pixels[i] = 0;
         pixels[i] =
             (colors[i % colors.length][0] << 16) |
             (colors[i % colors.length][1] << 8) |
             colors[i % colors.length][2];
+    }
+    console.log("pixels: ", pixels);
     ws281x.render(pixels);
 }
 
