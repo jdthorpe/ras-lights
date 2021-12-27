c7464cec4bf1c5bfcbc7d046ce7c9d7fd4ac6c91
Jason Thorpe <jdthorpe@gmail.com>
2021-12-27 13:53:55 -0800

    express server

--- show/build/app.js
+++ show/build/app.js
@@ -1,1 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const lights_1 = __importDefault(require("./src/routes/lights"));
+const app = (0, express_1.default)();
+const port = 5000;
+app.use(body_parser_1.default.json());
+app.use(lights_1.default);
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ show/build/src/routes/lights.js
@@ -0,0 +1,44 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = require("path");
+const fs_1 = require("fs");
+const perf_hooks_1 = require("perf_hooks");
+const express_1 = require("express");
+const ajv_1 = __importDefault(require("ajv"));
+const js_yaml_1 = __importDefault(require("js-yaml"));
+const ws681x_1 = require("../ws681x");
+const schema = js_yaml_1.default.load((0, fs_1.readFileSync)((0, path_1.join)(__dirname, "..", "..", "..", "config.ini"), "utf-8"));
+const ajv = new ajv_1.default();
+const router = (0, express_1.Router)();
+router.get("/off", (req, res) => {
+    const start = perf_hooks_1.performance.now();
+    (0, ws681x_1.turn_off)();
+    const end = perf_hooks_1.performance.now();
+    res.status(200);
+    res.send(`OK ${end - start}`);
+});
+router.get("/random", (req, res) => {
+    const start = perf_hooks_1.performance.now();
+    (0, ws681x_1.random_colors)();
+    const end = perf_hooks_1.performance.now();
+    console.log("Got body:", req.body);
+    res.status(200);
+    res.send(`OK ${end - start}`);
+});
+router.post("/set-colors", (req, res) => {
+    const body = req.body;
+    if (!ajv.validate(schema, body)) {
+        res.status(400);
+        res.send(`Body does not match the schema:\n${ajv.errors}`);
+        return;
+    }
+    const start = perf_hooks_1.performance.now();
+    (0, ws681x_1.set_colors)(body);
+    const end = perf_hooks_1.performance.now();
+    res.status(200);
+    res.send(`OK ${end - start}`);
+});
+exports.default = router;
