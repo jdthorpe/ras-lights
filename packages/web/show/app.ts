8424fe4b0058c893226ea82581fb15bf1826c817
Jason Thorpe <jdthorpe@gmail.com>
2021-12-27 13:53:55 -0800

    express server

---
+++ packages/web/show/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import bodyParser from "body-parser";
+import lights from "./src/routes/lights";
+
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.json());
+app.use(lights);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
