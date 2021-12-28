33fd56ba69f0bcfe0b5dadc626bf242076cb3197
Jason Thorpe <jdthorpe@gmail.com>
2021-12-27 18:29:56 -0800

    fix mode route

--- show/build/src/routes/mode.js
+++ show/build/src/routes/mode.js
@@ -5,14 +5,14 @@ const express_1 = require("express");
 const mode_1 = require("../mode");
 const index_1 = require("../../index");
 const router = (0, express_1.Router)();
-router.get("/mode/:mode", (req, res) => {
+router.get("/:mode", (req, res) => {
     const start = perf_hooks_1.performance.now();
     (0, mode_1.setMode)(req.params.mode);
     const end = perf_hooks_1.performance.now();
     res.status(200);
     res.send(`OK ${end - start}`);
 });
-router.get("/mode/", (req, res) => {
+router.get("/", (req, res) => {
     res.json(index_1.mode_config);
 });
 exports.default = router;
