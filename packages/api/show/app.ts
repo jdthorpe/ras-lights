bd9657ed45f827285db7ee9836db039487defdde
Jason Thorpe <jdthorpe@gmail.com>
2021-12-27 18:28:22 -0800

    fix mode route

--- packages/api/show/app.ts
+++ packages/api/show/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import lights from "./src/routes/lights";
+import mode from "./src/routes/mode";
 
 const app = express();
 const port = 5000;
 
 app.use(bodyParser.json());
 app.use("/lights", lights);
+app.use("/mode", mode);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
