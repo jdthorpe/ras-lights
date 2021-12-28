9133b61d81db0f0c27d61dc2edc87ace40b94a68
Jason Thorpe <jdthorpe@gmail.com>
2021-12-27 18:24:40 -0800

    mode config route

--- packages/web/show/build/src/routes/mode.js
+++ packages/web/show/build/src/routes/mode.js
@@ -1,1 +1,18 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const perf_hooks_1 = require("perf_hooks");
+const express_1 = require("express");
+const mode_1 = require("../mode");
+const index_1 = require("../../index");
+const router = (0, express_1.Router)();
+router.get("/mode/:mode", (req, res) => {
+    const start = perf_hooks_1.performance.now();
+    (0, mode_1.setMode)(req.params.mode);
+    const end = perf_hooks_1.performance.now();
+    res.status(200);
+    res.send(`OK ${end - start}`);
+});
+router.get("/mode/", (req, res) => {
+    res.json(index_1.mode_config);
+});
+exports.default = router;
