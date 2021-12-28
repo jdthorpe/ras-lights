52da6ac3c4a68aebe9713c431dce4b132da7f56e
Jason Thorpe <jdthorpe@gmail.com>
2021-12-27 18:28:22 -0800

    fix mode route

--- packages/web/show/build/app.js
+++ packages/web/show/build/app.js
@@ -6,10 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const lights_1 = __importDefault(require("./src/routes/lights"));
+const mode_1 = __importDefault(require("./src/routes/mode"));
 const app = (0, express_1.default)();
 const port = 5000;
 app.use(body_parser_1.default.json());
 app.use("/lights", lights_1.default);
+app.use("/mode", mode_1.default);
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
