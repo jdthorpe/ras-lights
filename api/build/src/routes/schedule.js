e771300671eb36a99f95dd219709afe10930f036
Jason Thorpe <jdthorpe@gmail.com>
2022-01-01 14:54:15 -0800

    delete route

--- api/build/src/routes/schedule.js
+++ api/build/src/routes/schedule.js
@@ -7,9 +7,9 @@ const perf_hooks_1 = require("perf_hooks");
 const express_1 = require("express");
 const nedb_promises_1 = __importDefault(require("nedb-promises"));
 /*
-curl -X POST https://192.168.4.64/schedule/
+curl http://192.168.4.64/api/schedule/
 
-curl -X POST https://192.168.4.64/schedule/ \
+curl -X POST http://192.168.4.64/api/schedule/ \
    -H 'Content-Type: application/json' \
    -d '{"name":"My Schedule","mode":"Rainbow Stripes","schedule":"30 8 * * 1-2,4-5"}'
 
@@ -17,7 +17,7 @@ curl -X POST https://192.168.4.64/schedule/ \
 const datastore = nedb_promises_1.default.create("/var/lib/ras-lights/schedule.db");
 const router = (0, express_1.Router)();
 router.get("/", async (req, res) => {
-    const results = await datastore.find({});
+    const results = await datastore.find({}, { _id: 0 });
     res.status(200);
     res.json(results);
 });
@@ -36,5 +36,20 @@ router.post("/", (req, res) => {
     const end = perf_hooks_1.performance.now();
     res.status(200);
     res.send(`OK ${end - start}`);
+});
+router.delete("/", (req, res) => {
+    const body = req.body;
+    console.log(`delete request body: ${JSON.stringify(body)}`);
+    // const start = performance.now();
+    // try {
+    //     datastore.update({ name: body.name }, body, { upsert: true });
+    // } catch (error) {
+    //     res.status(500);
+    //     res.send(`Failed to create a new schedule:\n${(error as any).message}`);
+    //     return;
+    // }
+    // const end = performance.now();
+    res.status(200);
+    // res.send(`OK ${end - start}`);
 });
 exports.default = router;
--- api/src/routes/schedule.ts
+++ api/src/routes/schedule.ts
@@ -3,9 +3,9 @@ import { Router, Request, Response } from "express";
 import Datastore from "nedb-promises";
 
 /*
-curl -X POST https://192.168.4.64/schedule/ 
+curl http://192.168.4.64/api/schedule/ 
 
-curl -X POST https://192.168.4.64/schedule/ \
+curl -X POST http://192.168.4.64/api/schedule/ \
    -H 'Content-Type: application/json' \
    -d '{"name":"My Schedule","mode":"Rainbow Stripes","schedule":"30 8 * * 1-2,4-5"}' 
 
@@ -15,7 +15,7 @@ const datastore = Datastore.create("/var/lib/ras-lights/schedule.db");
 const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
-    const results = await datastore.find({});
+    const results = await datastore.find({}, { _id: 0 });
     res.status(200);
     res.json(results);
 });
@@ -34,6 +34,22 @@ router.post("/", (req: Request, res: Response) => {
     const end = performance.now();
     res.status(200);
     res.send(`OK ${end - start}`);
+});
+
+router.delete("/", (req: Request, res: Response) => {
+    const body = req.body;
+    console.log(`delete request body: ${JSON.stringify(body)}`);
+    // const start = performance.now();
+    // try {
+    //     datastore.update({ name: body.name }, body, { upsert: true });
+    // } catch (error) {
+    //     res.status(500);
+    //     res.send(`Failed to create a new schedule:\n${(error as any).message}`);
+    //     return;
+    // }
+    // const end = performance.now();
+    res.status(200);
+    // res.send(`OK ${end - start}`);
 });
 
 export default router;
